bfe85d4166cb86f7035b06820513c542e912d0d1
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-12-12 19:14:11 +0100

    Add initial commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SpotifyAPI.Web;
+
+Console.WriteLine("Hello! Lets see whats new in your spotify account");
+
+// Creates default config for the Spotify client.
+var config = SpotifyClientConfig.CreateDefault();
+
+// Prepares the credentials request.
+var request = new ClientCredentialsRequest("YourClientId", "YourClientSecret");
+
+// Sends the request to the Spotify API.
+var response = await new OAuthClient(config).RequestToken(request);
+
+// Creates a new Spotify client with the access token.
+var spotify = new SpotifyClient(response.AccessToken); // "YourAccessToken"
+Console.WriteLine("Connected to Spotify!");
+
+// Gets the track with the given ID.
+var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+Console.WriteLine(track.Name);
---
+++ README.md
@@ -0,0 +1,6 @@
+# SpotifySongsTracker
+FavouritesSongsTracker helps users track their favorite Spotify playlists and songs. The app uses Spotify's OAuth for authentication, and users must provide their own credentials. No personal data is stored or shared, ensuring privacy and security
+
+STEPS:
+https://developer.spotify.com/documentation/web-api/concepts/access-token
+https://github.com/JohnnyCrazy/SpotifyAPI-NET?tab=readme-ov-file
