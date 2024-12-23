c59030173b355dfa98a8c9bb8c396b84d876f907
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2024-12-23 22:22:06 +0200

    Add AppResources

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿using SpotifyAPI.Web;
+using SpotifySongsTracker;
 using SpotifySongsTracker.Models;
 
 Console.WriteLine("Hello! Lets see whats new in your spotify account");
 
 var config = SpotifyClientConfig.CreateDefault();
-var request = new ClientCredentialsRequest("YourClientId", "YourClientSecret");
+var request = new ClientCredentialsRequest(AppResources.ClientId, AppResources.ClientSecret);
 var response = await new OAuthClient(config).RequestToken(request);
 
-var spotifyClient = new SpotifyClient(response.AccessToken); // "YourAccessToken"
+var spotifyClient = new SpotifyClient(response.AccessToken);
 Console.WriteLine("Connected to Spotify!");
 
-var track = await spotifyClient.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+var track = await spotifyClient.Tracks.Get("79QLNktJsGNRz4ijFnDywD");
 Song song = new()
 {
     Name = track.Name,
@@ -19,11 +20,26 @@ Song song = new()
     Duration = track.DurationMs
 };
 
-var playlist = await spotifyClient.Playlists.Get("4agdH8mVuilkTYMWxlU0o5");
-Playlist playlist1 = new()
+Console.WriteLine($"Song: {song.Name}, Album: {song.AlbumName}, Duration: {song.Duration}");
+
+var playlitst = await GetSpecificPlaylist();
+
+await GetSpecificPlaylist();
+
+async Task<Playlist> GetSpecificPlaylist()
 {
-    Id = playlist.Id,
-    Name = playlist.Name,
-    Genre = playlist.Description,
-    Songs = new List<Song> { song }
-};
+    var specificPlaylist = await spotifyClient.Playlists.Get("4agdH8mVuilkTYMWxlU0o5");
+    if (specificPlaylist == null)
+    {
+        Console.WriteLine("Playlist not found");
+        return null!;
+    }
+
+    return new()
+    {
+        Id = specificPlaylist!.Id ?? "",
+        Name = specificPlaylist!.Name ?? "",
+        Genre = specificPlaylist!.Description ?? "",
+        Songs = new List<Song> { song }
+    };
+}
